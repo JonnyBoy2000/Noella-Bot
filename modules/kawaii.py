3f46374f784aa78b6e121dad6e727e3fd1244e8c
Ashleeyy <ashley.muis@gmail.com>
2017-10-12 14:32:27 +0200

    Big Changes

--- modules/kawaii.py
+++ modules/kawaii.py
@@ -16,76 +16,62 @@ class Kawaii():
     @commands.command(pass_context = True, no_pm = True)
     async def hug(self, ctx, *, member : discord.Member = None):
 
+        author = ctx.author
         if not member:
-            choice = random.choice(hugs)
-            embed = discord.Embed(description = "**"+ ctx.author.mention +" you got hugged by" + self.bot.user.mention + "**", color = embed_color)
-            embed.set_image(url = "{}".format(choice))
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+            member = self.bot.user
 
-        else:
-            choice = random.choice(hugs) 
-            embed = discord.Embed(description = "**%s you got hugged by "%member.mention + ctx.author.mention +"**", color = embed_color)
-            embed.set_image(url = "{}".format(choice))
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+        choice = random.choice(hugs) 
+        embed = discord.Embed(description = f"**{member.name}** you got hugged by **{author.name}**", color = embed_color)
+        embed.set_image(url = f"{choice}")
+        await ctx.send(embed = embed)
+        await ctx.message.delete()
 
 #poke command (-poke [@mention])
     @commands.command(pass_context = True, no_pm = True)
     async def poke(self, ctx, *, member : discord.Member = None):
 
+        author = ctx.author
         if not member:
-            choice = random.choice(pokes) 
-            embed = discord.Embed(description = "**" + ctx.author.mention + " pokes " + self.bot.user.mention + "**", color = embed_color)
-            embed.set_image(url = "{}".format(choice))
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+            member = self.bot.user
         
-        else:
-            choice = random.choice(pokes) 
-            embed = discord.Embed(description = "**%s you got poked by "%member.mention + ctx.author.mention + "**", color = embed_color)
-            embed.set_image(url = "{}".format(choice))
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+        choice = random.choice(pokes) 
+        embed = discord.Embed(description = f"**{member.name}** you got poked by **{author.name}**", color = embed_color)
+        embed.set_image(url = f"{choice}")
+        await ctx.send(embed = embed)
+        await ctx.message.delete()
   
 #wave command (-wave [@mention])
     @commands.command(pass_context = True, no_pm = True)
     async def wave(self, ctx, *, member : discord.Member = None):
 
+        author = ctx.author
         if not member:
-            embed = discord.Embed(description = "**"+ ctx.author.mention +"** waves to **<@!365240645419270145>**", color = embed_color)
-            embed.set_image(url="https://i.imgur.com/w5kTICt.gif")
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+            member = self.bot.user
 
-        else:
-            embed = discord.Embed(description = "**"+ ctx.author.mention +"** waves at you **%s**"%member.mention, color = embed_color)
-            embed.set_image(url="https://i.imgur.com/w5kTICt.gif")
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+        embed = discord.Embed(description = f"**{author.name}** waves at you **{member.name}**", color = embed_color)
+        embed.set_image(url = "https://i.imgur.com/w5kTICt.gif")
+        await ctx.send(embed = embed)
+        await ctx.message.delete()
 
 #hide command (-hide [@mention])
     @commands.command(pass_context = True, no_pm = True)
     async def hide(self, ctx, *, member : discord.Member = None):
 
+        author = ctx.author
         if not member:
-            embed = discord.Embed(description = "**"+ ctx.author.mention +"** hides for **<@!365240645419270145>**", color = embed_color)
-            embed.set_image(url="https://i.imgur.com/BZQwbid.gif")
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+            member = self.bot.user
 
-        else:
-            embed = discord.Embed(description = "**"+ ctx.author.mention +"** hides for **%s**"%member.mention, color = embed_color)
-            embed.set_image(url="https://i.imgur.com/BZQwbid.gif")
-            await ctx.send(embed = embed)
-            await ctx.message.delete()
+        embed = discord.Embed(description = f"**{author.name}** is hiding for **{member.name}**", color = embed_color)
+        embed.set_image(url = "https://i.imgur.com/BZQwbid.gif")
+        await ctx.send(embed = embed)
+        await ctx.message.delete()
 
 #blush command (-blush)
     @commands.command(pass_context = True, no_pm = True)
     async def blush(self, ctx):
 
-        embed = discord.Embed(description = "**"+ ctx.author.mention +"** blushes", color = embed_color)
-        embed.set_image(url="https://i.imgur.com/DGhgJ1R.gif")
+        embed = discord.Embed(description = f"**{ctx.author.name}** is blushing!", color = embed_color)
+        embed.set_image(url = "https://i.imgur.com/DGhgJ1R.gif")
         await ctx.send(embed = embed)
         await ctx.message.delete()
         
@@ -93,8 +79,8 @@ class Kawaii():
     @commands.command(pass_context = True, no_pm = True)
     async def shine(self, ctx):
 
-        embed = discord.Embed(description = "**"+ ctx.author.mention +"** shines", color = embed_color)
-        embed.set_image(url="https://i.imgur.com/VUuoZfa.gif")
+        embed = discord.Embed(description = f"**{ctx.author.name}** is shining!", color = embed_color)
+        embed.set_image(url = "https://i.imgur.com/VUuoZfa.gif")
         await ctx.send(embed = embed)
         await ctx.message.delete()
 
@@ -102,8 +88,8 @@ class Kawaii():
     @commands.command(pass_context = True, no_pm = True)
     async def happy(self, ctx):
 
-        embed = discord.Embed(description = "**"+ ctx.author.mention +"** is super happy!", color = embed_color)
-        embed.set_image(url="https://i.imgur.com/4xSrwsj.gif")
+        embed = discord.Embed(description = f"**{ctx.author.name}** is super happy!", color = embed_color)
+        embed.set_image(url = "https://i.imgur.com/4xSrwsj.gif")
         await ctx.send(embed = embed)
         await ctx.message.delete()
 
