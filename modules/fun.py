8d21f53d6f04c01b0cdea480db9b51efa458edf1
Ashleeyy <ashley.muis@gmail.com>
2017-10-12 16:43:35 +0200

    Not much!

--- modules/fun.py
+++ modules/fun.py
@@ -42,7 +42,7 @@ class Fun():
 		author = ctx.author
 		if not member:
 			member = author
-		
+
 		embed = discord.Embed(title="Click here to visit *%s* profile!"%member.name, colour = embed_color, url="https://discordhub.com/profile/%s"%member.id, description="DiscordHub aims to provide a centralized platform for Discord\nby providing features such as user accounts and a public server list.")
 		embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/4.png")
 		embed.set_author(name="%s Profile!"%member.name, icon_url="https://cdn.discordapp.com/embed/avatars/4.png")
@@ -53,7 +53,7 @@ class Fun():
 
 	@commands.command(pass_context=True)
 	async def avatar(self, ctx, *, member : discord.Member = None):
-        
+
 		author = ctx.author
 		if not member:
 			member = author
@@ -79,10 +79,10 @@ class Fun():
 			embed.set_author(name = f"{osuplayer}'s Stats", url = f"https://osu.ppy.sh/u/{osuplayer}", icon_url = "https://s.ppy.sh/images/head-logo.png")
 			embed.set_footer(text = "Images provided by lemmmy.pw/osusig")
 			embed.set_image(url = f"http://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname={osuplayer}&pp=1&countryrank&flagshadow&flagstroke&opaqueavatar&avatarrounding=5&onlineindicator=undefined&xpbar&xpbarhex")
-			#embed.add_field(name="**"+ctx.author.name +"** asks:", inline=False, value="{}".format(question))
-			#embed.add_field(name="**Holy 8ball** answers:", inline=False, value="{}".format(choice))
 			await ctx.send(embed = embed)
 			await ctx.message.delete()
+
+#########################################
 
 def setup(bot):
     bot.add_cog(Fun(bot))
