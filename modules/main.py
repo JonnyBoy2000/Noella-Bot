b2a2e7bacfdfd1fb731cc271511ebca3e262d407
Ashley <t.muis94@gmail.com>
2017-10-18 03:51:22 +0200

    Removed Help Command from Mainfile

--- modules/main.py
+++ modules/main.py
@@ -20,29 +20,6 @@ async def on_ready():
     time_log = dt.utcnow().__format__('%H:%M:%S')
     await send_log.send(f"[{time_log}] - **{bot.user.name}** is online now!")
 
-##########################################
-############## Help Command ##############
-##########################################
-
-    @bot.command(pass_context = True, aliases=['h'])
-    async def help(ctx):
-        embed = discord.Embed(title=f"Command List for {bot.user.name}!", colour = embed_color, description=f"Prefix for {ctx.guild}: **{bot_prefix}**\nIf a command is not working, or something goes wrong?\nUse the this command `{bot_prefix}ctdev [question/feedback]`!\n**Don't include the example brackets when using the commands!**⠀\n⠀")
-        embed.set_thumbnail(url = bot.user.avatar_url)
-        embed.add_field(name="Core Commands", value=f"{bot_prefix}h or {bot_prefix}help \n{bot_prefix}invite \n{bot_prefix}ls or {bot_prefix}listservers \n{bot_prefix}si or {bot_prefix}serverinfo \n{bot_prefix}about \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="⠀", value=f"To get help and commands per PM \nTo get a PM with the Bot Invite \nTo get a list of server with {bot.user.name} \nTo see server information \nTo see information about {bot.user.name} \n", inline=True)
-        embed.add_field(name="Kawaii Commands", value=f"{bot_prefix}hug [@mention]⠀\n{bot_prefix}poke [@mention]⠀\n{bot_prefix}wave [@mention]⠀\n{bot_prefix}hide [@mention]⠀\n{bot_prefix}blush⠀\n{bot_prefix}shine⠀\n{bot_prefix}happy \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="⠀", value=f"To hug {bot.user.name} or someone else! \nTo poke {bot.user.name} or someone else! \nTo wave at {bot.user.name} or someone else! \nTo hide for {bot.user.name} or someone else! \nTo express that you're blushing! \nTo express you're shining! \nTo express you're happy! \n", inline=True)
-        embed.add_field(name="Fun Commands", value=f"{bot_prefix}8ball [question]⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="⠀", value="Ask all you want, to the holy 8Ball!\n", inline=True)
-        embed.add_field(name="Voice Commands", value=f"{bot_prefix}connect⠀\n{bot_prefix}disconnect \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="⠀", value=f"Connect {bot.user.name} [voice-channel]\nDisconnect {bot.user.name} [voice-channel]\n", inline=True)
-        embed.add_field(name="Administrator Commands", value=f"{bot_prefix}sr [@mention] [rolename]\n{bot_prefix}rr [@mention] [rolename] \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", inline=True)
-        embed.add_field(name="⠀", value="Give/set a role to someone!\nRemove a role from someone!\n", inline=True)
-        await ctx.author.send(embed = embed),
-
-        embed = discord.Embed(description = f"**{ctx.author.name}**, a personal message with all my commands is on the way!", color = embed_color_succes)
-        await ctx.send(embed = embed)
-
 #################################################
 ############## Contact Dev Command ##############
 #################################################
@@ -87,7 +64,7 @@ async def on_ready():
     async def latency(ctx):
         pingms = "{}".format(int(bot.latency * 1000))
         pings = "{}".format(int(bot.latency * 1))
-        await ctx.send("Ping - Calculating some shit in the background... beep beep...")
+        message = await ctx.send("Ping - Calculating some shit in the background... beep beep...")
         await asyncio.sleep(3)
         await message.edit(content = f"Pong! - My latency is **{pings}**s | **{pingms}**ms")
 
