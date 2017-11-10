a35bc7f9dc77b767406df6916dd52a0bcb80fb88
Ashley <t.muis94@gmail.com>
2017-11-10 06:58:09 +0100

    Added: Heart Reacuibs Data

---
+++ cogs/data/reactionsdata.py
@@ -0,0 +1,6 @@
+hearts = [
+'***Steals the heart and runs away*** **(>°o°)>♡**',
+'***Aawwwwe so cute*** **(♡◡♡)**',
+'***Smells love in the air*** **(♡◡♡)**'
+f'***{self.bot.user.name} gets love too?*** **(⺣◡⺣)♡**'
+]
