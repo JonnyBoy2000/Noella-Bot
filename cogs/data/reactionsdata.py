c3a19e0173449e77c703f6f1d1764e93a80e66ef
Ashley <t.muis94@gmail.com>
2017-11-10 07:41:03 +0100

    added a comma... arrgg

--- cogs/data/reactionsdata.py
+++ cogs/data/reactionsdata.py
@@ -1,6 +1,6 @@
 hearts = [
 '***Steals the heart and runs away*** **(>°o°)>♡**',
 '***Aawwwwe so cute*** **(♡◡♡)**',
-'***Smells love in the air*** **(♡◡♡)**'
-f'***Do I get some love too?*** **(⺣◡⺣)♡**'
+'***Smells love in the air*** **(♡◡♡)**',
+'***Do I get some love too?*** **(⺣◡⺣)♡**'
 ]
