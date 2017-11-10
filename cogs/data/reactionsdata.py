f75eb29403b5d538e34a490d13492ad2e47cc851
Ashley <t.muis94@gmail.com>
2017-11-10 07:01:15 +0100

    Little fix

--- cogs/data/reactionsdata.py
+++ cogs/data/reactionsdata.py
@@ -2,5 +2,5 @@ hearts = [
 '***Steals the heart and runs away*** **(>°o°)>♡**',
 '***Aawwwwe so cute*** **(♡◡♡)**',
 '***Smells love in the air*** **(♡◡♡)**'
-f'***{self.bot.user.name} gets love too?*** **(⺣◡⺣)♡**'
+f'***{bot.user.name} gets love too?*** **(⺣◡⺣)♡**'
 ]
